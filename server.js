1956e6a825717558fa1e93306bd77aa53716e068
djecov484 <djecov484@gmail.com>
2021-09-15 18:41:43 -0700

    final commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const drinks= require('./models/drinks');
+const food = require('./models/food');
 
 app.get('', (req,res) => {
     res.send('Welcome to the Gitpub App!');
@@ -10,6 +11,18 @@ app.get('/drinks', (req,res) => {
     res.render('drinks_index.ejs', {allDrinks:drinks});
 })
 
+app.get('/drinks/:id', (req,res)=> {
+    res.render('drinks_show.ejs', {drink: drinks[req.params.id]});
+})
+
+// foods
+app.get('/food', (req,res) => {
+    res.render('food_index.ejs', {allFood:food});
+})
+
+app.get('/food/:id', (req,res)=> {
+    res.render('food_show.ejs', {food: food[req.params.id]});
+})
 
 
 app.listen(3000, () => {
